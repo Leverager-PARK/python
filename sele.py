09df7e9fc9ee1bf6718c7792f4cad8dd6177362b
sanghwan park <psh000987@naver.com>
2022-11-22 17:44:31 +0900

    완료

---
+++ sele.py
@@ -0,0 +1,7 @@
+from selenium import webdriver
+driver = webdriver.Chrome()
+url = 'https://accounts.hancom.com/oauth2/authorize?client_id=5xusimMCZbLJQIfLl06i&redirect_uri=https%3A%2F%2Fwww.hancomdocs.com%2Flogin&response_type=code&scope=hancomOauth%3Awrite+hancomOauth%3Aread&state=eyJ0IjoiMTY2OTA3NDU5NzkwNyIsInJlZGlyZWN0IjoiL215ZHJpdmUvc2hhcmVzIn0%3D&token=true'
+driver.get(url)
+
+while(True):
+    pass
